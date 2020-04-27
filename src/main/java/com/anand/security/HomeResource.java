9fea85c33cfb4367c6762dd2322ca78b2dde9426
ANAND <anand.javaj2ee@gmail.com>
2020-04-27 12:31:48 +0530

    spring authentication configuration

---
+++ src/main/java/com/anand/security/HomeResource.java
@@ -0,0 +1,31 @@
+package com.anand.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    // http://localhost:8080/
+    // Will auto redirected to http://localhost:8080/login.
+    // To sign in, enter
+    // Username: user
+    // Password: 430cdbac-0ddb-414a-8457-12bbc62e3822 (Go to your Spring Boot log and find the line: "Using generated security password:......"
+    // When you set your username and password in application.properties file, you enter your own username and password.
+    // When using proper file like create a class called SecurityConfiguration extends WebSecurityConfigurerAdapter.
+    // In memory authentication, you also enter your own username and password.
+    // You can log out, by using http://localhost:8080/logout. It will ask you to confirm logout. Click yes and you're logged out.
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Welcome</h1>";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "<h1>Welcome User</h1>";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "<h1>Welcome Admin</h1>";
+    }
+}
---
+++ src/test/java/com/anand/security/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.anand.security;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
